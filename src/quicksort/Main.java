e90fc33abb9754476a3a0ad6cf2427e01497929c
Moisés Breton <moisesbreton11@gmail.com>
2022-10-31 23:00:53 -0600

    funciona

---
+++ src/quicksort/Main.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package quicksort;
+
+/**
+ *
+ * @author Berke
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+     int [] arreglo = {5,2,6,12,6,8,1};
+     Ordenador o = new Ordenador ();
+     o.ordenarQuicksort (arreglo);
+     
+        for(int i = 0; i <arreglo.length; i++)
+        {
+            
+        
+        System.out.println(arreglo [i]);
+    }
+    
+}
+    
+}
+        
